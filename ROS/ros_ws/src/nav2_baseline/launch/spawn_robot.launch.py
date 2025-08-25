6f60fa8ccfb854ce5cf1283c528389a60d586612
Marcus Reis <marcusreispestalozzi2@gmail.com>
2025-08-25 17:10:06 -0300

    to be aborted

--- ROS/ros_ws/src/nav2_baseline/launch/spawn_robot.launch.py
+++ ROS/ros_ws/src/nav2_baseline/launch/spawn_robot.launch.py
@@ -1,9 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from math import pi
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+
+    model_file = os.path.join(
+        get_package_share_directory('nav2_baseline'),
+        'model_bumper', 'model.sdf')
+    
+    # Original model.sdf:
+    # '/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf'
 
     # Turtlebot3 node
     spawn_robot = Node(
@@ -11,7 +20,7 @@ def generate_launch_description():
         executable='spawn_entity.py',
         arguments=[
             '-entity', 'turtlebot3',
-            '-file', '/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf',
+            '-file', model_file,
             '-x', '4.5',   # 4.5; 9.0
             '-y', '0.0',  # -4.5; 0.0
             '-z', '0.01',
