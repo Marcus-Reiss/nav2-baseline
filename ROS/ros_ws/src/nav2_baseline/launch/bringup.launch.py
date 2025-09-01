badfcb2810dc104568872e4c1e1cc16030bf5e64
Marcus Reis <marcusreispestalozzi2@gmail.com>
2025-08-31 23:24:41 -0300

    Worlds added, recs recorded

--- ROS/ros_ws/src/nav2_baseline/launch/bringup.launch.py
+++ ROS/ros_ws/src/nav2_baseline/launch/bringup.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -9,11 +10,21 @@ def generate_launch_description():
     pkg = get_package_share_directory('nav2_baseline')
     turtlebot3_pkg = get_package_share_directory('turtlebot3_bringup')
 
+    default_world = 'corridor_3x10_dynamic_v0.world'
+    world_name = LaunchConfiguration('world_name')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            name='world_name',
+            default_value=default_world
+        ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(pkg, 'launch', 'gazebo_custom_world.launch.py')
-            )
+            ),
+            launch_arguments={
+                'world_name': world_name
+            }.items()
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
--- ROS/ros_ws/src/nav2_baseline/launch/gazebo_custom_world.launch.py
+++ ROS/ros_ws/src/nav2_baseline/launch/gazebo_custom_world.launch.py
@@ -1,20 +1,30 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
+    pkg = get_package_share_directory('nav2_baseline')
     gazebo_pkg = get_package_share_directory('gazebo_ros')
-    world_path = os.path.join(get_package_share_directory('nav2_baseline'), 
-                              'worlds', 'corridor_3x10_dynamic_v2.world')
-    
+
+    default_world = 'corridor_3x10_dynamic_v0.world'
+    world_name = LaunchConfiguration('world_name')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            name='world_name',
+            default_value=default_world,
+            description='Name of the .world file in nav2_baseline/worlds'
+        ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(
                 gazebo_pkg, 'launch', 'gazebo.launch.py'
             )),
-            launch_arguments={'world': world_path}.items()
+            launch_arguments={
+                'world': PathJoinSubstitution([pkg, 'worlds', world_name])
+            }.items()
         ),
     ])
